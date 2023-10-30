b2bb1afad240a5b50eaeb71c5266b29399d83da4
maksim-chernydev <maksim-chernydev@DESKTOP-U2UTPU1>
2023-10-30 21:56:30 +0300

    Добавил ограничение на размер

--- lw5/Command/Command/Image.cpp
+++ lw5/Command/Command/Image.cpp
@@ -1,11 +1,12 @@
 #include "stdafx.h"
 #include "Image.h"
 
+void ValidateSize(int width, int height);
+
 CImage::CImage(const Path& path, int width, int height)
 	: m_path(path)
-	, m_width(width)
-	, m_height(height)
 {
+	Resize(width, height);
 };
 
 Path CImage::GetPath()const
@@ -25,6 +26,18 @@ int CImage::GetHeight()const
 
 void CImage::Resize(int width, int height)
 {
+	ValidateSize(width, height);
 	m_width = width;
 	m_height = height;
 };
+
+void ValidateSize(int width, int height)
+{
+	if (width > 10000 || width < 1 || height > 10000 || height < 1)
+	{
+		std::stringstream ss;
+		ss << "image size must be at range from 1 to 10000. Given width and height: "
+			<< width << " and " << height;
+		throw std::logic_error(ss.str());
+	}
+}
