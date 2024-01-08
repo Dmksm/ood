60ca1fa89cf8bec8e22ebbe0ec05cb6ec59f4055
maksim-chernydev <maksim-chernydev@DESKTOP-U2UTPU1>
2024-01-08 12:55:25 +0300

    обновил разрешение

--- lw10/lw10/lw10/main.cpp
+++ lw10/lw10/lw10/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 	sf::RenderWindow renderWindow(sf::VideoMode::getDesktopMode(), "The Shape",
-		sf::Style::Default, sf::ContextSettings(24, 8, 16, 4, 6, 0, true));
+		sf::Style::Fullscreen, sf::ContextSettings(24, 8, 16, 4, 6, 0, true));
 	Picture picture;
 	CController controller(std::make_unique<Picture>(std::move(picture)),
 		&renderWindow,
